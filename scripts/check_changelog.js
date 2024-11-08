b921d4f8700665cb69d38af6bbc91dec06e031fe
whitemoshui <sme_moshui@outlook.com>
2024-11-08 16:53:46 +0800

    remove yellow color

--- scripts/check_changelog.js
+++ scripts/check_changelog.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const colors = {
 	red: text => `\x1b[31m${text}\x1b[0m`,
 	green: text => `\x1b[32m${text}\x1b[0m`,
-	yellow: text => `\x1b[33m${text}\x1b[0m`,
 };
 
 try {
